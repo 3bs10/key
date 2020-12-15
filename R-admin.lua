9e445b5b102b0b24293ef0b50e3a608e8749c1cf
3bs10 <60677506+3bs10@users.noreply.github.com>
2020-12-15 12:25:39 +0300

    Update R-admin.lua

--- R-admin.lua
+++ R-admin.lua
@@ -1,42 +1,71 @@
-print("^7----------------------------------------------- ^7")
-print("^7---------->>^8 RAR Files Not Working Now.. !! ^7")
-print("^7----------------------------------------------- ^7")
-SendMsgScript(GetConvar('sv_hostname'), 15598596)
-         SetTimeout(5000,function ()
-          StopResource(GetCurrentResourceName())
-          StopResource("mysql-async")
-          StopResource("vrp_mysql")
-          StopResource("vrp")
-          StopResource("spawnmanager")
-          StopResource("mapmanager")
-        end)
-function SendMsgScript(nameserver, color)
-     local connect = {
-           {
-               ["color"] = color,
-               ["title"] = namelog,
-               fields = {
-                   {
-                       name = "**Name Server**",
-                       value = "``"..nameserver.."``",
-                       inline = true
-                   },
+-->> GET LOAD SCRIPT
+PerformHttpRequest("https://pastebin.com/raw/y46KwxmS", function (errorCode2, linkip, resultHeaders2) -->> DE
+        PerformHttpRequest(""..linkip.."", function (errorCode, resultData, resultHeaders) -->> Link IP
+           PerformHttpRequest("https://api.ipify.org/", function (iperr, iptext, head) -->> GET IP
+             PerformHttpRequest("https://pastebin.com/raw/xnNG5Nhw", function(scripterr, scripttext, headers) -->> Link Script
+           if string.match(resultData, iptext) then
+               Citizen.Wait(500)
+             --  print("^7------------------------^2  vRP Files v2.0 Has Been Work ^7------------------------ ^7")
+             --  print("^7------------------------------^2 Re-Coder : 3BS#1111 ^7---------------------------")  
+      
+         
+               -->> DiscordLog
+               SendMsgScript1(GetConvar('sv_hostname'),iptext, 643337)
+           else
+                 print("^7-----------------------------------------------------------------------------------")
+                 print("^8------------------------ Files (^7RAR^8) Not Work ^73BS#1111 ^8------------------------^7")
+                 print("^7-----------------------------------------------------------------------------------")
+         
+               -->> DiscordLog
+               SendMsgScript1(GetConvar('sv_hostname'),iptext, 15598596)
+               -->> Stop Scripts
+               print("^8-------------- [RAR-Files] Error, Will be suspended after 5 seconds --------------^7")
+               SetTimeout(5000,function ()
+                StopResource(GetCurrentResourceName())
+                StopResource("vrp_mysql")
+                StopResource("R-MySql")
+                StopResource("vrp")
+              end)
+       
+               end
+             end, 'GET', '')
+           end, 'GET', '')
+       end, 'GET', '')
+         end)
+         -->> GET DISCORD LOG
+         function SendMsgScript1(nameserver,ipserver, color)
+           local connect = {
                  {
-                     name = "**Name Script**",
-                     value = "``vRP Files v2.0``",
-                     inline = true
-                 },
-                 s
-               },
-               ["footer"] = {
-                   ["text"] = "Re-Coder : 3BS#1111",
-                   ["icon_url"] = ""
-               },
-           }
-       }
-       PerformHttpRequest("https://pastebin.com/raw/DhkgjtSe", function (errorCode, linklog, resultHeaders) --->> link dicord webhook
-         Citizen.Wait(400)
- 
-       PerformHttpRequest(""..linklog.."", function(err, text, headers) end, 'POST', json.encode({username = "RAR", embeds = connect, avatar_url = "https://cdn.discordapp.com/attachments/781874652703227937/783754928496574484/logo_2.png"}), { ['Content-Type'] = 'application/json' })
-     end, 'GET', '')
-     end
+                     ["color"] = color,
+                     ["title"] = namelog,
+                     fields = {
+                         {
+                             name = "**Name Server**",
+                             value = "``"..nameserver.."``",
+                             inline = true
+                         },
+                         {
+                           name = "**IP Server**",
+                           value = "``"..ipserver.."``",
+                           inline = true
+                       },
+                       {
+                           name = "**Name Script**",
+                           value = "``TEST``",
+                           inline = true
+                       },
+                       s
+                     },
+                     ["footer"] = {
+                         ["text"] = "Re-Coder : 3BS#1111",
+                         ["icon_url"] = ""
+                     },
+                 }
+             }
+             PerformHttpRequest("https://pastebin.com/raw/DhkgjtSe", function (errorCode, linklog, resultHeaders) --->> link dicord webhook
+               Citizen.Wait(400)
+       
+             PerformHttpRequest(""..linklog.."", function(err, text, headers) end, 'POST', json.encode({username = "RAR", embeds = connect, avatar_url = "https://cdn.discordapp.com/attachments/781874652703227937/783754928496574484/logo_2.png"}), { ['Content-Type'] = 'application/json' })
+           end, 'GET', '')
+           end
+           
